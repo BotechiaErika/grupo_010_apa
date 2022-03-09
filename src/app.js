eb6d8f741812b6945fe8c1999694c5dbd720f315
BotechiaErika <mendoncabotechiaerikaluisa@gmail.com>
2022-03-09 11:13:38 -0300

    errores con correeccion en proximos commits

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 // var createError = require('http-errors');
 const express = require('express');
+<<<<<<< HEAD
 // var path = require('path');
 // var cookieParser = require('cookie-parser');
 // var logger = require('morgan');
@@ -7,11 +8,23 @@ const express = require('express');
 const mainRouters = require('./routes/mainControllers');
 //const usersRouters = require('./routes/usersControllers');
 const path = require('path')
+=======
+const path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+const port = process.env.PORT||9694
+
+const mainRouters = require('./routes/mainRoutes.js')
+const productsRouters = require('./routes/productsRoutes.js')
+// const usuariosRouters = require('./routes/usuariosRoutes.js')
+//const usersRouters = require('./routes/usersControllers');
+>>>>>>> 9119b16 (errores con correeccion en proximos commits)
 const app = express();
 app.use(express.static(path.join(__dirname, './../public')));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+<<<<<<< HEAD
 
 // app.use(logger('dev'));
 // app.use(express.json());
@@ -25,6 +38,23 @@ app.use('/', mainRouters);
 // // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
 //   next(createError(404));
+=======
+// ACTUALIZANDO ENTRY POINT CLASSE CRUD
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+// app.use(logger('dev'));
+
+
+// app.use(cookieParser());
+
+
+app.use('/' , mainRouters)
+app.use('/productos' , productsRouters);
+// app.use('/usuarios' , usuariosRouters);
+
+// // catch 404 and forward to error handler
+app.use('/*' , (req, res) => {res.render('error404')})
+>>>>>>> 9119b16 (errores con correeccion en proximos commits)
 // });
 //
 // // error handler
@@ -38,9 +68,15 @@ app.use('/', mainRouters);
 //   res.render('error');
 // });
 
+<<<<<<< HEAD
 
 app.listen(process.env.port||9694), ()=>{
     console.log('server corriendo port 9696')
 }
+=======
+app.listen(port , () => {
+    console.log('server corriendo port 9694')
+})
+>>>>>>> 9119b16 (errores con correeccion en proximos commits)
 
 module.exports = app;
