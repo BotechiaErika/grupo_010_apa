c3792da49a9e5a80d24db567abd5939c394d4ecb
BotechiaErika <mendoncabotechiaerikaluisa@gmail.com>
2022-03-11 03:25:53 -0300

    salvar

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -1,9 +1,15 @@
 let controller = {
     indexPage: (req, res) => {
-        res.render('index', {title: 'APA'});
+        res.render('index', { title: 'APA' });
     },
-    landingUser: (req, res) => {
-        res.render('users/landingpageUser', {title: 'HOME'});
+    landingUser: (req, res) =>{
+        res.render('users/landingpageUser', { title: 'HOME' });
+    },
+    crearCuenta: (req, res) =>{
+        res.render('users/cadastroUsuarios', { title: 'CREAR CUENTA' });
+    },
+    iniciarSeccion: (req, res) =>{
+        res.render('users/login', { title: 'INICIAR SECCION' });
     }
 }
 
